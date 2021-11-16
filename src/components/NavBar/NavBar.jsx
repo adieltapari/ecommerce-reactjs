e8fa7b47edffd316b86d8d79d7f645feed508e52
adieltapari <adiel.tapari@gmail.com>
2021-11-16 19:37:07 -0300

    create NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="container">
+      <ul>
+        <li>
+          <a href="https://www.google.com">Galeria</a>
+        </li>
+        <li>
+          <a href="https://www.google.com">Inicio</a>
+        </li>
+        <li>
+          <a href="https://www.google.com">Enlace</a>
+        </li>
+        <li>
+          <a href="https://www.google.com">Enlace</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
