9d6ce0a0308da9864fb0d53e4a8b8e3848a54d66
adieltapari <adiel.tapari@gmail.com>
2021-12-15 12:24:44 -0300

    hide shopping cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = () => {
     return (
         <>
             <div >
-                {items.length === 0 && <p> carrito vacio</p>}
+                {items.length === 0 && <h4 style={{ margin: 40 }}> carrito vacio</h4>}
                 {
                     items.map((item) => (
                         <div className="ContainerCart" key={item.id}>
@@ -26,12 +26,14 @@ const Cart = () => {
                     ))
                 }
             </div >
-            <div className='EmptyCart'>
-                <div className="ui button " style={{ margin: "10" }} onClick={() => clearItems()}>
-                    <i className="cart icon"></i>
-                    vaciar carrito
+            {items.length !== 0 &&
+                <div className='EmptyCart'>
+                    <div className="ui button " style={{ margin: "10" }} onClick={() => clearItems()}>
+                        <i className="cart icon"></i>
+                        vaciar carrito
+                    </div>
                 </div>
-            </div>
+            }
         </>
 
     )
