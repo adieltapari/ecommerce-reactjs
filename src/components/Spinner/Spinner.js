0ce4e6c5576a37867d31f75f1c75d10a7e46579f
adieltapari <adiel.tapari@gmail.com>
2022-12-19 13:57:58 -0300

    fix spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Loader, Segment } from 'semantic-ui-react';
+import React from "react";
+import { Loader, Segment } from "semantic-ui-react";
 
 const Spinner = () => {
-
-    return (
-        <Segment>
-            <Loader size='medium' active inline='centered'  >Cargando</Loader>;
-        </Segment>
-    )
-}
+  return (
+    <Segment>
+      <Loader size="large" active inline="centered">
+        Cargando
+      </Loader>
+    </Segment>
+  );
+};
 
 export default Spinner;
